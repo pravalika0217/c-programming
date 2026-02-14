d7ce41ed9679d1cae3b983b2d7f585139d111d58
pravalika0217 <dasaripravalika36@gmail.com>
2026-02-14 14:33:31 +0530

    Add files via upload

---
+++ range.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    int r1;
+    int r2;
+    printf("enter the number \n");
+    scanf("%d",&num);
+    printf("enter the start of range: \n");
+    scanf("%d",&r1);
+    printf("enter the start of range: \n");
+    scanf("%d",&r2);
+    int count=0; 
+    for(int i=r1;i<=10;i++)
+    {
+        if(num%i==0)
+        {
+            count++;
+        }
+    }
+    printf("count=%d",count);
+    return 0;
+}
